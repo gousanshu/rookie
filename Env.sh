7e18679ce27177509a88782b14fca7658634075f
gousanshu <guxf1476@ifohoo.com>
2020-05-26 13:00:35 +0800

    ssh

--- Env.sh
+++ Env.sh
@@ -7,9 +7,18 @@ yum_config(){
 	yum clean all && yum makecache
 }
 
+ssh_config(){
+	sed -i 's/#Port 22/Port 222/' /etc/ssh/sshd_config
+	systemctl restart sshd.service
+	semanage port -a -t ssh_port_t -p tcp 222
+	firewall-cmd --zone=public --add-port=222/tcp --permanent
+	systemctl start firewalld.service 
+}
+
 
 if [ $UID -eq 0 ];then
 	yum_config
+	ssh_config
 else
 	echo "您不是root管理员，没有权限安装"
 fi
